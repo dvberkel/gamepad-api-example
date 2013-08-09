89971d8a2ca46552a4028b1d124a0502092f3c10
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-08-09 14:53:40 +0200

    list connected gamepads

--- js/gamepad.js
+++ js/gamepad.js
@@ -8,4 +8,21 @@
     var paragraph = document.createElement('p');
     paragraph.textContent = 'gamepad api supported: ' + gamepadSupported();
     body.appendChild(paragraph);
+
+    if (gamepadSupported()) {
+	var listOfGamepads = document.createElement('ul');
+	var gamepads = navigator.webkitGetGamepads();
+	for (var index = 0; index < gamepads.length; index++) {
+	    var gamepad = gamepads.item(index);
+	    if (!!gamepad) {
+		console.log(gamepads.item(index));
+		var gamepadItem = document.createElement('li');
+		gamepadItem.textContent = gamepad.id;
+		listOfGamepads.appendChild(gamepadItem);
+	    }
+
+	}
+	body.appendChild(listOfGamepads);
+    }
+
 })(window, document, navigator);
