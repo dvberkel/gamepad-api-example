90b0fc9d5d1b4ba803eed281d9895995a8c5229e
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-08-09 14:32:32 +0200

    test if gamepad api is supported

--- js/gamepad.js
+++ js/gamepad.js
@@ -1,7 +1,11 @@
-(function(window, document){
+(function(window, document, navigator){
+    function gamepadSupported() {
+	return !!navigator.webkitGetGamepads
+    }
+
     var body = document.getElementsByTagName('body')[0];
 
     var paragraph = document.createElement('p');
-    paragraph.textContent = 'gamepad api';
+    paragraph.textContent = 'gamepad api supported: ' + gamepadSupported();
     body.appendChild(paragraph);
-})(window, document);
+})(window, document, navigator);
