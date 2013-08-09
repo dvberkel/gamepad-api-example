3d1e0725894ca14d5f4af93afad9cb472604cbbb
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-08-09 14:28:00 +0200

    incorporate javascript

---
+++ js/gamepad.js
@@ -0,0 +1,7 @@
+(function(window, document){
+    var body = document.getElementsByTagName('body')[0];
+
+    var paragraph = document.createElement('p');
+    paragraph.textContent = 'gamepad api';
+    body.appendChild(paragraph);
+})(window, document);
